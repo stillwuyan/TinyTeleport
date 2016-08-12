01324f474c2a5145151b96f876e38752e5549285
stillwuyan <stillwuyan@126.com>
2016-08-12 19:23:46 +0800

    chore(refactor): use swig and script to download

--- downloader/multidownloader.cpp
+++ downloader/multidownloader.cpp
@@ -1,43 +1,18 @@
 // MultiDownloader.cpp : Defines the entry point for the application.
 //
 
-#include "stdafx.h"
-#include <WinSock2.h>
-#include <WS2tcpip.h>
-#include <Urlmon.h>
-#include <atlstr.h>
-#include <WinIoCtl.h>
-#include <assert.h>
-#include "MultiDownloader.h"
-#include "icseapi.h"
-
 #pragma comment(lib, "Ws2_32.lib")
 
-#define MAX_LOADSTRING 100
-
-// Global Variables:
-HINSTANCE hInst;								// current instance
-TCHAR szTitle[MAX_LOADSTRING];					// The title bar text
-TCHAR szWindowClass[MAX_LOADSTRING];			// the main window class name
+TCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
+TCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
 HWND g_hWndMain = NULL;
 HWND g_hWndURL = NULL;
 HWND g_hWndFile = NULL;
-WNDPROC g_oldURLProc;
-WNDPROC g_oldFileProc;
 
-// Forward declarations of functions included in this code module:
-ATOM				MyRegisterClass(HINSTANCE hInstance);
-BOOL				InitInstance(HINSTANCE, int);
-LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK	OpenURLDlgProc(HWND, UINT, WPARAM, LPARAM);
-LRESULT CALLBACK    ControlWindowsProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
-HRESULT             OpenURL(HWND);
-void                ThreadProc(void* param);
-void                OnDownloadProgress(HWND, WPARAM, LPARAM);
-
+#define MAX_LOADSTRING 100
 #define MAX_URL_LENGTH 512
 #define DEFAULT_BUFFER_LENGTH 1024
+#define WM_DOWNLOAD_PROGRESS WM_USER + 1
 
 WCHAR g_wcFile[MAX_URL_LENGTH] = L"C:\\Users\\test\\Desktop\\download.jpg";
 WCHAR g_wcURL[MAX_URL_LENGTH] = L"http://www.test.com/img/1.jpg";
@@ -46,123 +21,6 @@ char g_acDomain[MAX_URL_LENGTH] = {0};
 int g_factor = 10;
 __int64 g_segmentMatrix[10][3] = {{0}};
 
-#define WM_DOWNLOAD_PROGRESS WM_USER + 1
-
-int APIENTRY _tWinMain(HINSTANCE hInstance,
-                     HINSTANCE hPrevInstance,
-                     LPTSTR    lpCmdLine,
-                     int       nCmdShow)
-{
-	UNREFERENCED_PARAMETER(hPrevInstance);
-	UNREFERENCED_PARAMETER(lpCmdLine);
-
-    WSADATA wsaData;
-    int iResult = 0;
-    iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
-    if (iResult != 0)
-    {
-        ICSE_OUTPUTDEBUGSTRING_W(L"WSAStartup failed: %d", iResult);
-        return E_FAIL;
-    }
-
- 	// TODO: Place code here.
-	MSG msg;
-	HACCEL hAccelTable;
-
-	// Initialize global strings
-	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-	LoadString(hInstance, IDC_MULTIDOWNLOADER, szWindowClass, MAX_LOADSTRING);
-	MyRegisterClass(hInstance);
-
-	// Perform application initialization:
-	if (!InitInstance (hInstance, nCmdShow))
-	{
-		return FALSE;
-	}
-
-	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_MULTIDOWNLOADER));
-
-	// Main message loop:
-	while (GetMessage(&msg, NULL, 0, 0))
-	{
-		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-		{
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
-		}
-	}
-
-    WSACleanup();
-
-	return (int) msg.wParam;
-}
-
-
-
-//
-//  FUNCTION: MyRegisterClass()
-//
-//  PURPOSE: Registers the window class.
-//
-//  COMMENTS:
-//
-//    This function and its usage are only necessary if you want this code
-//    to be compatible with Win32 systems prior to the 'RegisterClassEx'
-//    function that was added to Windows 95. It is important to call this function
-//    so that the application will get 'well formed' small icons associated
-//    with it.
-//
-ATOM MyRegisterClass(HINSTANCE hInstance)
-{
-	WNDCLASSEX wcex;
-
-	wcex.cbSize = sizeof(WNDCLASSEX);
-
-	wcex.style			= CS_HREDRAW | CS_VREDRAW;
-	wcex.lpfnWndProc	= WndProc;
-	wcex.cbClsExtra		= 0;
-	wcex.cbWndExtra		= 0;
-	wcex.hInstance		= hInstance;
-	wcex.hIcon			= LoadIcon(hInstance, MAKEINTRESOURCE(IDI_MULTIDOWNLOADER));
-	wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
-	wcex.hbrBackground	= (HBRUSH)(COLOR_WINDOW+1);
-	wcex.lpszMenuName	= MAKEINTRESOURCE(IDC_MULTIDOWNLOADER);
-	wcex.lpszClassName	= szWindowClass;
-	wcex.hIconSm		= LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
-
-	return RegisterClassEx(&wcex);
-}
-
-//
-//   FUNCTION: InitInstance(HINSTANCE, int)
-//
-//   PURPOSE: Saves instance handle and creates main window
-//
-//   COMMENTS:
-//
-//        In this function, we save the instance handle in a global variable and
-//        create and display the main program window.
-//
-BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
-{
-   HWND hWnd;
-
-   hInst = hInstance; // Store instance handle in our global variable
-
-   hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, 800, 600, NULL, NULL, hInstance, NULL);
-
-   if (!hWnd)
-   {
-      return FALSE;
-   }
-
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
-
-   g_hWndMain = hWnd;
-   return TRUE;
-}
 
 //
 //  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
@@ -176,31 +34,6 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 //
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-	int wmId, wmEvent;
-	PAINTSTRUCT ps;
-	HDC hdc;
-
-	switch (message)
-	{
-	case WM_COMMAND:
-		wmId    = LOWORD(wParam);
-		wmEvent = HIWORD(wParam);
-		// Parse the menu selections:
-		switch (wmId)
-		{
-		case IDM_ABOUT:
-			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-			break;
-		case IDM_EXIT:
-			DestroyWindow(hWnd);
-			break;
-        case IDM_OPENURL:
-            OpenURL(hWnd);
-            break;
-		default:
-			return DefWindowProc(hWnd, message, wParam, lParam);
-		}
-		break;
 	case WM_PAINT:
 		hdc = BeginPaint(hWnd, &ps);
 		// TODO: Add any drawing code here...
@@ -216,92 +49,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         }
         EndPaint(hWnd, &ps);
 		break;
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		break;
-    case WM_DOWNLOAD_PROGRESS:
-        OnDownloadProgress(hWnd, wParam, lParam);
-        break;
-	default:
-		return DefWindowProc(hWnd, message, wParam, lParam);
-	}
-	return 0;
-}
-
-// Message handler for about box.
-INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
-{
-	UNREFERENCED_PARAMETER(lParam);
-	switch (message)
-	{
-	case WM_INITDIALOG:
-		return (INT_PTR)TRUE;
-
-	case WM_COMMAND:
-		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-		{
-			EndDialog(hDlg, LOWORD(wParam));
-			return (INT_PTR)TRUE;
-		}
-		break;
-	}
-	return (INT_PTR)FALSE;
-}
-
-INT_PTR CALLBACK OpenURLDlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    UNREFERENCED_PARAMETER(lParam);
-    switch (message)
-    {
-    case WM_INITDIALOG:
-        SetDlgItemText(hDlg, IDC_URL, g_wcURL);
-        SetDlgItemText(hDlg, IDC_FILE, g_wcFile);
-
-        g_hWndURL = GetDlgItem(hDlg, IDC_URL);
-        g_hWndFile = GetDlgItem(hDlg, IDC_FILE);
-        SetFocus(g_hWndURL);
-        g_oldURLProc = (WNDPROC)GetWindowLong(g_hWndURL, GWL_WNDPROC);
-        SetWindowLong(g_hWndURL, GWL_WNDPROC, (LONG)&ControlWindowsProc);
-        g_oldFileProc = (WNDPROC)GetWindowLong(g_hWndFile, GWL_WNDPROC);
-        SetWindowLong(g_hWndFile, GWL_WNDPROC, (LONG)&ControlWindowsProc);
-        return (INT_PTR)FALSE;
-
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK)
-        {
-            GetDlgItemText(hDlg, IDC_URL, g_wcURL, MAX_URL_LENGTH);
-            GetDlgItemText(hDlg, IDC_FILE, g_wcFile, MAX_URL_LENGTH);
-            EndDialog(hDlg, LOWORD(wParam));
-            return (INT_PTR)TRUE;
-        }
-        else if (LOWORD(wParam) == IDCANCEL)
-        {
-            EndDialog(hDlg, LOWORD(wParam));
-            return (INT_PTR)FALSE;
-        }
-        break;
-    }
-    return (INT_PTR)FALSE;
-}
-
-LRESULT CALLBACK ControlWindowsProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    if ((message == WM_KEYDOWN) && (wParam == 0x41/*A*/) && ((GetAsyncKeyState(VK_CONTROL) & 0x8000) == 0x8000))
-    {
-        SendMessage(hWnd, EM_SETSEL, 0, -1);
-    }
-    if (hWnd == g_hWndURL)
-    {
-        return g_oldURLProc(hWnd, message, wParam, lParam);
-    }
-    else if (hWnd == g_hWndFile)
-    {
-        return g_oldFileProc(hWnd, message, wParam, lParam);
-    }
-    else
-    {
-        assert(FALSE);
-    }
 }
 
 HRESULT OpenURL(HWND hWnd)
@@ -582,13 +329,4 @@ LOOP:
 
     ICSE_OUTPUTDEBUGSTRING_W(L"[%d]recv finish", index);
     closesocket(connectSocket);
-}
-
-void OnDownloadProgress(HWND hWnd, WPARAM wParam, LPARAM lParam)
-{
-    // TODO.
-    //int index = (int)wParam;
-    //__int64 length = (__int64)lParam;
-    InvalidateRect(hWnd, NULL, TRUE);
-    return;
 }
